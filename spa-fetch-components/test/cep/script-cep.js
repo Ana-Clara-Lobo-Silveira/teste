f28fca58ac9f2c6e5832cf0d8270262de0460d3f
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-04-30 11:20:16 -0300

    Passagem do arquivo cep

---
+++ spa-fetch-components/test/cep/script-cep.js
@@ -0,0 +1,17 @@
+
+/* Criar uma requisição HTTP com fetch e .then */
+const url = 'https://viacep.com.br/ws/01011000/json/';
+const consulta = fetch(url)
+consulta
+.then((resposta) => { 
+    if(!resposta.ok){
+        throw new Error('Erro na requisição.')
+    }
+    return resposta.json()})
+.then((dados) => {
+    if(dados.erro){
+        throw new Error('Verifique o CEP novamente!')
+    }
+    console.log(dados)})
+.catch((error) =>{console.warn(error.message)});
+
